317ff0fef22adbe6b8c0d6cd61616f5d94451893
Sujit Maharjan <shubhajeet.per@gmail.com>
2024-01-08 14:08:47 -0600

    apply permutation

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -4,7 +4,26 @@ from test_framework import generic_test
 
 
 def apply_permutation(perm: List[int], A: List[int]) -> None:
-    # TODO - you fill in here.
+    # TODO - you fill in here.'
+    # pos = {i: i for i in range(len(A))}
+    # copyarr = []
+    # for i in range(len(perm)):
+    #     copyarr.append(A[perm[i]])
+    # print(copyarr)
+    # A = copyarr
+    # print(A)
+    # for i in range(len(perm)):
+    #     acutual_pos = pos[perm[i]]
+    #     A[i], A[acutual_pos] = A[acutual_pos], A[i]
+    #     pos[acutual_pos] = i
+    #     pos[i] = acutual_pos
+    for i in range(len(A)):
+        next = i
+        while perm[next] >= 0:
+            A[i], A[perm[next]] = A[perm[next]], A[i]
+            tmp = perm[next]
+            perm[next] = perm[next] - len(perm)
+            next = tmp
     return
 
 
