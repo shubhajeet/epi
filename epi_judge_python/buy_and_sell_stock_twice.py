349214c76b92aa4fcd01e4ceeb0bb94c9aa3685c
Sujit Maharjan <shubhajeet.per@gmail.com>
2024-01-08 12:04:00 -0600

    prime sieve

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -5,7 +5,29 @@ from test_framework import generic_test
 
 def buy_and_sell_stock_twice(prices: List[float]) -> float:
     # TODO - you fill in here.
-    return 0.0
+    first_buy_sell_profits = [0.0] * len(prices)
+    second_buy_sell_profits = [0.0] * len(prices)
+    min_price = 0
+    for i in range(len(prices)):
+        profit = prices[i] - min_price
+        first_buy_sell_profits[i] = max(profit, 0)
+        if prices[i] < min_price:
+            min_price = prices[i]
+    max_price = prices[-1]
+    for i in reversed(range(len(prices))):
+        profit = max_price - prices[i]
+        second_buy_sell_profits[i] = max(profit, 0)
+        if prices[i] > max_price:
+            max_price = prices[i]
+    max_profit = 0
+    max_profits = []
+    for i in range(len(prices)-1):
+        profit = first_buy_sell_profits[i] + second_buy_sell_profits[i+1]
+        max_profits.append(profit)
+        if profit > max_profit:
+            max_profit = profit
+    print(max_profits)
+    return max_profit
 
 
 if __name__ == '__main__':
--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -6,7 +6,17 @@ from test_framework import generic_test
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
     # TODO - you fill in here.
-    return []
+    is_prime = [True] * (n+1)
+    is_prime[0] = False
+    is_prime[1] = False
+    primes = []
+    for i in range(2, n+1):
+        if is_prime[i]:
+            primes.append(i)
+            for j in range(2, n//i+1):
+                is_prime[j*i] = False
+    # print(primes)
+    return primes
 
 
 if __name__ == '__main__':
