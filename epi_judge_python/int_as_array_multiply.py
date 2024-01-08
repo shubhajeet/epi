60633217f7066260cdf5724f1b59c3bb0b333b9e
Sujit Maharjan <shubhajeet.per@gmail.com>
2024-01-07 18:32:24 -0600

    int as array multiply

--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -8,12 +8,18 @@ def multiply(num1: List[int], num2: List[int]) -> List[int]:
     result = [0 for i in range(len(num1) + len(num2))]
     for i in range(len(num1)):
         for j in range(len(num2)):
-            result[i + j + 1] += num1[i] * num2[j]
+            result[i + j + 1] += abs(num1[i] * num2[j])
     for i in reversed(range(len(result))):
         if result[i] >= 10:
             result[i - 1] += result[i] // 10
             result[i] %= 10
-    print(result)
+    for i in range(len(result)):
+        if result[i] != 0:
+            break
+    result = result[i:]
+    if num1[0] * num2[0] < 0:
+        result[0] *= -1
+    # print(result)
     return result
 
 
