55fd0580e232cd29dae0232235f72f60dbab2ce5
Sujit Maharjan <shubhajeet.per@gmail.com>
2024-01-17 10:47:24 -0600

    is palindromic

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -3,6 +3,10 @@ from test_framework import generic_test
 
 def is_palindromic(s: str) -> bool:
     # TODO - you fill in here.
+    half = len(s) // 2
+    for i in range(half):
+        if s[i] != s[-(i+1)]:
+            return False
     return True
 
 
