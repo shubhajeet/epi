8453eb88f8b1f11c2d1eb7b98f75851d6f754d62
Sujit Maharjan <shubhajeet.per@gmail.com>
2024-01-18 11:46:30 -0600

    roman_to_integer

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -3,7 +3,16 @@ from test_framework import generic_test
 
 def roman_to_integer(s: str) -> int:
     # TODO - you fill in here.
-    return 0
+    roman_values = {"I": (0, 1), "V": (1, 5), "X": (2, 10), "L": (
+        3, 50), "C": (4, 100), "D": (5, 500), "M": (6, 1000)}
+    roman_char = ["I", "V", "X", "L", "C", "D", "M", None, None]
+    num = 0
+    for i in range(len(s)):
+        if i < len(s)-1 and (s[i+1] == roman_char[roman_values[s[i]][0]+1] or s[i+1] == roman_char[roman_values[s[i]][0]+2]):
+            num -= roman_values[s[i]][1]
+        else:
+            num += roman_values[s[i]][1]
+    return num
 
 
 if __name__ == '__main__':
